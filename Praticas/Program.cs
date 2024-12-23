c8d1a34c7636e79c6e5061d946afd5b5849278df
Guilherme Lima <ghduep@outlook.com>
2024-12-23 10:20:41 -0300

    alteração no program.cs

--- Praticas/Program.cs
+++ Praticas/Program.cs
@@ -1,8 +1,1 @@
-﻿using PetHouse.Models;
-
-Medico medico = new("Guilherme Lima", "1234567", Especialidade.CARDIOLOGIA);
-Dono dono = new("Paloma Duarte", "30245479803", "ghduep@outlook.com");
-Pet pet = new("Lua", "Gato", 1, dono);
-
-Consulta consulta = new(pet, medico, DateTime.Now);
-Console.WriteLine(consulta.Descricao);
+﻿// TESTE O CODIGO :) //
